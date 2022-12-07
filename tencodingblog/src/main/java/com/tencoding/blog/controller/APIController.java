41f014bc8768d20d8bff3a07224397251dd5d6a0
iw_br <iw_br@naver.com>
2022-12-07 18:01:00 +0900

    spring 환경설정

---
+++ tencodingblog/src/main/java/com/tencoding/blog/controller/APIController.java
@@ -0,0 +1,16 @@
+package com.tencoding.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class APIController {
+
+	@GetMapping("/get")
+	public String get() { 
+		return "test";
+	}
+	
+}
