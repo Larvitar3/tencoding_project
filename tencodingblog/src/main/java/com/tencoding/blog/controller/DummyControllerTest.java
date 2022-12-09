4132356591779bbca9e7e150f64fb7ce6763c719
iw_br <iw_br@naver.com>
2022-12-09 11:21:49 +0900

    더티체킹 사용해보기

--- tencodingblog/src/main/java/com/tencoding/blog/controller/DummyControllerTest.java
+++ tencodingblog/src/main/java/com/tencoding/blog/controller/DummyControllerTest.java
@@ -2,6 +2,8 @@ package com.tencoding.blog.controller;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +47,7 @@ public class DummyControllerTest {
 		
 	}
 
+	// 회원 조회
 	@GetMapping("/user/{id}")
 	public User detail(@PathVariable int id) {
 
@@ -60,6 +64,7 @@ public class DummyControllerTest {
 		return user;
 	}
 
+	// 회원 가입
 	// http://localhost:9090/blog/dummy/signup
 	@PostMapping("/signup")
 	public String signUp(@RequestBody User user) {
@@ -72,5 +77,50 @@ public class DummyControllerTest {
 		return "회원가입이 완료되었습니다";
 
 	}
+	
+	// 회원 수정
+	@Transactional // 작업의 단위. 함수 종료시에 commit이 된다
+	// 함수 종료시에 더티체킹을 하고 수정된 데이터가 있다면 commit이 된다.
+	@PutMapping("/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User reqUser) {
+		
+		log.info(">> id : {}, >>>password {}, email >>>> {}",
+				id, reqUser.getPassword(), reqUser.getEmail());
+		
+		// 1. 지정 사용자가 있는지 확인.
+		// 2_1. 사용자가 있다면 넘겨받은 데이터로 수정 후 저장
+		// 2_2. 사용자가 없다면 클라이언트에게 잘못된 요청임을 알림
+		
+		User user = userRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("수정에 실패 하였습니다.");
+		});
+		
+		// 수정할 데이터의 값 받아오기
+		user.setPassword(reqUser.getPassword());
+		user.setEmail(reqUser.getEmail());
+		
+		// 수정한 데이터의 값을 저장하기
+//		userRepository.save(user);
+		
+		return null;
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
