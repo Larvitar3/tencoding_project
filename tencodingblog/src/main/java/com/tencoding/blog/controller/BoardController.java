e819fd7240c27dc2ebbfbfe96633bdca45d37fe1
iw_br <iw_br@naver.com>
2022-12-19 14:00:30 +0900

    페이징 처리

--- tencodingblog/src/main/java/com/tencoding/blog/controller/BoardController.java
+++ tencodingblog/src/main/java/com/tencoding/blog/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.tencoding.blog.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,13 +24,37 @@ public class BoardController {
 
 	@GetMapping({ "", "/" })
 	public String index(Model model,
-			@PageableDefault(size = 1, sort = "id", direction = Direction.DESC) Pageable pageble) {
+			@PageableDefault(size = 2, sort = "id", direction = Direction.DESC) Pageable pageble) {
 
 		Page<Board> boards = boardService.getBoardList(pageble);
+
+		int PAGENATION_BLOCK_COUNT = 2;
+
 		// page.first / true, false
 		// page.last / true, false
-		model.addAttribute("boards", boards);
+//		model.addAttribute("boards", boards);
 		// jsp 파일에서 model 추상객체를 이용해 컨트롤러에게 내려 준 데이터 접근이 가능
+
+		int nowPage = boards.getPageable().getPageNumber() + 1;
+		int startPage = Math.max(nowPage - PAGENATION_BLOCK_COUNT, 1);
+		int endPage = Math.min(nowPage + PAGENATION_BLOCK_COUNT, boards.getTotalPages());
+
+		System.out.println("화면에 보여주는 게시물 갯수 :: " + boards.getSize());
+		System.out.println("전체 게시물 크기 :: " + boards.getTotalPages());
+		System.out.println("Now : " + nowPage + "start!" + startPage);
+		System.out.println(" end : " + endPage);
+
+		ArrayList<Integer> pageNumbers = new ArrayList<>();
+
+		model.addAttribute("boards", boards);
+		model.addAttribute("nowPage", nowPage);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
+		model.addAttribute("pageNumbers", pageNumbers);
+
+		for (int i = startPage; i <= endPage; i++) {
+			pageNumbers.add(i);
+		}
 
 		return "index";
 	}
@@ -38,28 +64,21 @@ public class BoardController {
 
 		return "/board/save_form";
 	}
-	
+
 	@GetMapping("/board/{id}")
 	public String showDetail(@PathVariable int id, Model model) {
-		
+
 		model.addAttribute("board", boardService.boardDetail(id));
-		
+
 		return "/board/detail";
 	}
-	
-	@GetMapping("/board/{id}/update_form") //mapping의 변수명과 매개변수의 변수명을 다르게 하고싶은경우 name을 사용한다.
-	public String updateForm(@PathVariable (name = "id")int borderId, Model model) {
-		
+
+	@GetMapping("/board/{id}/update_form") // mapping의 변수명과 매개변수의 변수명을 다르게 하고싶은경우 name을 사용한다.
+	public String updateForm(@PathVariable(name = "id") int borderId, Model model) {
+
 		model.addAttribute("board", boardService.boardDetail(borderId));
-		
+
 		return "/board/update_form";
 	}
-	
-	
-	
-	
-	
-	
-	
 
 }
--- tencodingblog/src/main/webapp/WEB-INF/views/index.jsp
+++ tencodingblog/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,6 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ include file="layout/header.jsp"%>
-
 <c:forEach var="board" items="${boards.content}">
 	<div>
 		<div class="card m-2">
@@ -13,17 +12,25 @@
 </c:forEach>
 <br />
 
-<!-- <ul class="pagination justify-content-center">
-	<li class="page-item"><a class="page-link" href="?page=${boards.number - 1}">Previous</a></li>
-	<li class="page-item"><a class="page-link" href="?page= ${boards.number + 1}">Next</a></li>
-</ul> -->
-
 <!-- page.first
 	 page.last -->
 <ul class="pagination justify-content-center">
 	<c:set var="isDisabled" value="disabled"></c:set>
 	<c:set var="isNotDisabled" value=" "></c:set>
 	<li class="page-item ${boards.first ? isDisabled : isNotDisabled}"><a class="page-link" href="?page=${boards.number - 1}">Previous</a></li>
+	
+	<c:forEach var="num" items="${pageNumbers}">
+		<c:choose>
+			<c:when test="${nowPage eq num}">
+				<li class="page-item active"><a class="page-link" href="?page=${num-1}"> ${num} </a></li>
+			</c:when>
+			<c:otherwise>
+				<li class="page-item"><a class="page-link" href="?page=${num-1}"> ${num} </a></li>
+			</c:otherwise>
+		</c:choose>
+	</c:forEach>
+	
+<!-- 	<li class="page-item active"><a class="page-link" href="#">2</a></li> -->
 	<li class="page-item ${boards.last ? isDisabled : isNotDisabled}"><a class="page-link" href="?page=${boards.number + 1}">Next</a></li>
 </ul>
 <%@ include file="layout/footer.jsp"%>
